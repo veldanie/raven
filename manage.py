3f61f4e239504553383a9a3b11e6a4667e2e6a0b
veldanie <davelasquez@gmail.com>
2018-06-04 11:40:49 -0500

    application instead of manager

--- manage.py
+++ manage.py
@@ -2,21 +2,21 @@
 import os
 from app import create_app, db
 from app.models import User, Role
-from flask_script import Manager, Shell
+from flask_script import application, Shell
 from flask_migrate import Migrate, MigrateCommand
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-manager = Manager(app)
+application = Manager(app)
 migrate = Migrate(app, db)
 
 def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role)
 
-manager.add_command("shell", Shell(make_context=make_shell_context))
-manager.add_command('db', MigrateCommand)
+application.add_command("shell", Shell(make_context=make_shell_context))
+application.add_command('db', MigrateCommand)
 
 
-@manager.command
+@application.command
 def deploy():
     """Run deployment tasks."""
     from flask.ext.migrate import upgrade
@@ -33,4 +33,4 @@ def deploy():
 
 
 if __name__ == '__main__':
-    manager.run()
+    application.run()
